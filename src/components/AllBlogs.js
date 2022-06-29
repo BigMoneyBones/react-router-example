67fc6204eb5b58074f076a830769ecdf715de36a
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-06-29 13:07:23 -0500

    update

--- src/components/AllBlogs.js
+++ src/components/AllBlogs.js
@@ -1,25 +1,68 @@
 import React from "react";
 
-// the useSearchParams hook of React Router returns an array with two elements: the first is the current location’s search params and the latter is a function
-// that can be used to update them.
-import { useSearchParams } from "react-router-dom";
+// the useSearchParams hook of React Router returns an array with two elements:
+// the first is the current location’s search params and the latter is a
+// function that can be used to update them.
+import { useSearchParams, Outlet } from "react-router-dom";
 
 // {blogPosts} is accessible because in App.js it is passed into the <Allblogs /> component as props.
 const AllBlogs = ({ blogPosts }) => {
-  let [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortOrder = searchParams.get("sortOrder") || "asc";
+  const sortField = searchParams.get("sortField") || "createdAt";
+  const limit = searchParams.get("limit") || 0;
+  const page = searchParams.get("page") || 0;
+  const copyOfBlogPosts = blogPosts;
+
+  const compare = (a, b) => {
+    if (sortOrder.toLowerCase() === "asc") {
+      if (a[sortField] < b[sortField]) {
+        return -1;
+      }
+      if (a[sortField] > b[sortField]) {
+        return 1;
+      }
+      return 0;
+    }
+    if (sortOrder.toLowerCase() === "desc") {
+      if (a[sortField] < b[sortField]) {
+        return 1;
+      }
+      if (a[sortField] > b[sortField]) {
+        return -1;
+      }
+      return 0;
+    }
+  };
+
+  const limitPage = (blogs) => {
+    let blogIndex = limit * page;
+    let returnBlogs = [];
+    for (let i = 0; i < limit; i++) {
+      if (blogs[blogIndex]) {
+        returnBlogs.push(blogs[blogIndex]);
+      }
+      blogIndex++;
+    }
+    return returnBlogs;
+  };
+
+  const sortedBlogPosts = copyOfBlogPosts.sort(compare);
+  const limitPageBlogs = limitPage(sortedBlogPosts);
 
   return (
     <>
-      AllBlogs
+      &nbsp;
+      <div>All Blogs</div>
       <ul>
-        {blogPosts.map((blog, index) => {
+        {limitPageBlogs.map((blog, index) => {
           return (
             <li key={index}>
-              <p className="blog-elements">Title: {blog.title}</p>
-              <p className="blog-elements">Author: {blog.author}</p>
-              <p className="blog-elements">Text: {blog.text}</p>
-              <p className="blog-elements">CreatedAt: {blog.createdAt}</p>
-              <p className="blog-elements">ID: {blog.id}</p>
+              <p className="text">Title: {blog.title}</p>
+              <p className="text">Author: {blog.author}</p>
+              <p className="text">Text: {blog.text}</p>
+              <p className="text">Date Created: {blog.createdAt}</p>
+              <p className="text">ID: {blog.id}</p>
             </li>
           );
         })}
